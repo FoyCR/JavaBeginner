0ee05e2a822a61677d23b55d0aff4ca40bf0c630
Foy <ryocr000@gmail.com>
2024-02-20 11:37:49 -0600

    Add GuessTheLetter
    First version of a guess a letter simple game

---
+++ src/GuessTheLetter.java
@@ -0,0 +1,15 @@
+/**
+ * Little game to guess a letter. demonstrate the use of 'if' sentence and read
+ * the user input
+ */
+public class GuessTheLetter {
+    public static void main(String[] args)
+            throws java.io.IOException {
+        char ch, answer = 'k';
+        System.out.println("Do your best guessing a letter between a and z");
+        ch = (char) System.in.read();
+
+        if (ch == answer)
+            System.out.println("** Right **");
+    }
+}
