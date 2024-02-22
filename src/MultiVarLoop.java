84e9d73ace3eb72001e4f0af9dbe296c69994e51
Foy <ryocr000@gmail.com>
2024-02-22 15:21:47 -0600

    Add MultiVarLoop
    Demonstrate multiple variables in a for..loop statement

---
+++ src/MultiVarLoop.java
@@ -0,0 +1,8 @@
+public class MultiVarLoop {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0, j = 10; i <= j; i++, j--) {// note the comma separator
+            System.out.println("i and j: " + i + "  " + j);
+        }
+    }
+}
